dbb1c42762c95a44df0bc41da5700b2cf621d24b
pdmonge <parthamonge@gmail.com>
2017-09-12 17:19:24 -0600

    Minor mods. Merging before re-pull

---
+++ javascript/auth.js
@@ -0,0 +1,21 @@
+// FIREBASE
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyA3CVxnIXyQgnfPZPp1EJsMD1W0xEIGTtw",
+  authDomain: "dubootcamp-group-project1.firebaseapp.com",
+  databaseURL: "https://dubootcamp-group-project1.firebaseio.com",
+  projectId: "dubootcamp-group-project1",
+  storageBucket: "",
+  messagingSenderId: "93086047570"
+};
+firebase.initializeApp(config);
+
+
+
+
+
+
+document.ready(function() {
+
+
+});
