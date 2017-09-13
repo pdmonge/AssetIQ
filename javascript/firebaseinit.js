8da052799cb8985d20d80c85ae37a189e613ebb3
pdmonge <parthamonge@gmail.com>
2017-09-12 21:05:50 -0600

    Add firebaseinit.js. Removed register from index.html

--- javascript/firebaseinit.js
+++ javascript/firebaseinit.js
@@ -1,4 +1,3 @@
-// FIREBASE
 // Initialize Firebase
 var config = {
   apiKey: "AIzaSyA3CVxnIXyQgnfPZPp1EJsMD1W0xEIGTtw",
@@ -9,27 +8,3 @@ var config = {
   messagingSenderId: "93086047570"
 };
 firebase.initializeApp(config);
-
-function validateInput() {
-	return true;
-}
-
-$('#loginButtonSubmit').click({
-
-
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  // var errorCode = error.code;
-  // var errorMessage = error.message;
-  // ...
-  
-	});
-});
-
-
-
-
-document.ready(function() {
-
-
-});
