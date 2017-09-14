e4f433d11418a530b229157e235932d1c31294ee
Willas88 <robertwmathison@gmail.com>
2017-09-14 17:50:00 -0600

    googlemapsAPI

--- javascript/inventory.js
+++ javascript/inventory.js
@@ -1,97 +1,92 @@
-// FIREBASE
-// Initialize Firebase
-// var config = {
-//   apiKey: "AIzaSyA3CVxnIXyQgnfPZPp1EJsMD1W0xEIGTtw",
-//   authDomain: "dubootcamp-group-project1.firebaseapp.com",
-//   databaseURL: "https://dubootcamp-group-project1.firebaseio.com",
-//   projectId: "dubootcamp-group-project1",
-//   storageBucket: "",
-//   messagingSenderId: "93086047570"
-// };
+var config = {
+    apiKey: "AIzaSyCtZ5Ob4IWwqG5UbqHESm1_sCjQaypfexs",
+    authDomain: "assetiq-4d15d.firebaseapp.com",
+    databaseURL: "https://assetiq-4d15d.firebaseio.com",
+    projectId: "assetiq-4d15d",
+    storageBucket: "assetiq-4d15d.appspot.com",
+    messagingSenderId: "300194767147"
+};
+firebase.initializeApp(config);
 
-// firebase.initializeApp(config);
-
-// REMOVED FIREBASE INITIALIZATION FROM INVENTORY.JS SINCE THERE NOW EXISTS FIREBASEINIT.JS
-// REFERENCED ON ALL HTML PAGES -MKC
 
 $(document).ready(function() {
 
-// Firebase
+    // Firebase
 
-//eBay API
-function _cb_findItemsByKeywords(response) {
-    console.log(response);
-    var idx = 0;
-    for (idx = 0; idx < response.findItemsByKeywordsResponse[0].searchResult[0].item.length; idx++) {
-        var item = response.findItemsByKeywordsResponse[0].searchResult[0].item[idx];
-        var imageUrl = item.galleryURL[0];
-        var title = item.title;
-  
+    //eBay API
+    function _cb_findItemsByKeywords(response) {
+        console.log(response);
+        var idx = 0;
+        for (idx = 0; idx < response.findItemsByKeywordsResponse[0].searchResult[0].item.length; idx++) {
+            var item = response.findItemsByKeywordsResponse[0].searchResult[0].item[idx];
+            var imageUrl = item.galleryURL[0];
+            var title = item.title;
 
-        $('#table-body').append('<tr><td><img src="' + imageUrl + '"></td><td>' + title + '</td></tr>');
+
+            $('#table-body').append('<tr><td><img src="' + imageUrl + '"></td><td>' + title + '</td></tr>');
+        }
     }
-}
 
-var rawurlencode = function (str) {
-    str = (str + '');
-    return encodeURIComponent(str)
-        .replace(/!/g, '%21')
-        .replace(/'/g, '%27')
-        .replace(/\(/g, '%28')
-        .replace(/\)/g, '%29')
-        .replace(/\*/g, '%2A')
-};
+    var rawurlencode = function(str) {
+        str = (str + '');
+        return encodeURIComponent(str)
+            .replace(/!/g, '%21')
+            .replace(/'/g, '%27')
+            .replace(/\(/g, '%28')
+            .replace(/\)/g, '%29')
+            .replace(/\*/g, '%2A')
+    };
 
-var params = {
-    "OPERATION-NAME": "findItemsByKeywords",
-    "SERVICE-VERSION": "1.0.0",
-    "SECURITY-APPNAME": "deidrang-DUcode-PRD-88e35c535-a6b95781",
-    "GLOBAL-ID": "EBAY-US",
-    "RESPONSE-DATA-FORMAT": "JSON",
-    "callback": "_cb_findItemsByKeywords",
-    "REST-PAYLOAD": null,
-    "keywords": "laptop", //keywork lookup
-    "paginationInput.entriesPerPage": "10", //number if items returned
-};
-var keys = Object.keys(params);
-keys.sort();
-var list = [];
-var i;
-for (i = 0; i < keys.length; i++) {
-    var key = keys[i];
-    if (params[key]) {
-        list.push(rawurlencode(key) + '=' + rawurlencode(params[key]));
-    } else {
-        list.push(rawurlencode(key));
+    var params = {
+        "OPERATION-NAME": "findItemsByKeywords",
+        "SERVICE-VERSION": "1.0.0",
+        "SECURITY-APPNAME": "deidrang-DUcode-PRD-88e35c535-a6b95781",
+        "GLOBAL-ID": "EBAY-US",
+        "RESPONSE-DATA-FORMAT": "JSON",
+        "callback": "_cb_findItemsByKeywords",
+        "REST-PAYLOAD": null,
+        "keywords": "laptop", //keywork lookup
+        "paginationInput.entriesPerPage": "10", //number if items returned
+    };
+    var keys = Object.keys(params);
+    keys.sort();
+    var list = [];
+    var i;
+    for (i = 0; i < keys.length; i++) {
+        var key = keys[i];
+        if (params[key]) {
+            list.push(rawurlencode(key) + '=' + rawurlencode(params[key]));
+        } else {
+            list.push(rawurlencode(key));
+        }
     }
-}
-var canonical_query_string = list.join('&');
+    var canonical_query_string = list.join('&');
 
-var uri = "/services/search/FindingService/v1";
-var endpoint = "svcs.ebay.com";
+    var uri = "/services/search/FindingService/v1";
+    var endpoint = "svcs.ebay.com";
 
-var request_url = 'http://' + endpoint + uri + '?' + canonical_query_string;
+    var request_url = 'http://' + endpoint + uri + '?' + canonical_query_string;
 
-console.log('request_url = ' + request_url);
+    console.log('request_url = ' + request_url);
 
-$.ajax({
-    url: request_url,
-    type: "GET",
-    dataType: "jsonp",
-    jsonp: false,
-    cache: true,
-    crossDomain: true,
-});
+    $.ajax({
+        url: request_url,
+        type: "GET",
+        dataType: "jsonp",
+        jsonp: false,
+        cache: true,
+        crossDomain: true,
+    });
 
-// Logout Button
-$("#logout-button").on("click", function() {
-    event.preventDefault();
-    // var currentUser = Parse.User.current();
+    // Logout Button
+    $("#logout-button").on("click", function() {
+        event.preventDefault();
+        // var currentUser = Parse.User.current();
         // if (currentUser) {
         //     Parse.User.logout();
         //     window.location="Sign_In.html";
         // } else {
-            window.location = "index.html";
-});
+        window.location = "index.html";
+    });
 
 });
--- javascript/people.js
+++ javascript/people.js
@@ -1,91 +1,146 @@
+//Needs to Be done: Asset ID will be queried from inventory page then populate
+//in the dropdown box for selection prior to form submission
 
-// // =======
-// var config = {
-//     apiKey: "AIzaSyCtZ5Ob4IWwqG5UbqHESm1_sCjQaypfexs",
-//     authDomain: "assetiq-4d15d.firebaseapp.com",
-//     databaseURL: "https://assetiq-4d15d.firebaseio.com",
-//     projectId: "assetiq-4d15d",
-//     storageBucket: "assetiq-4d15d.appspot.com",
-//     messagingSenderId: "300194767147"
 
-// };
-
-// firebase.initializeApp(config);
-
-// REMOVED FIREBASE INITIALIZATION FROM PEOPLE.JS SINCE THERE NOW EXISTS FIREBASEINIT.JS
-// REFERENCED ON ALL HTML PAGES - MKC
+//Initialize Firebase through external link
 
 $(document).ready(function() {
 
-// Firebase
-var firstName, lastName, name, position, department, email, address, phone, assetID;
 
-// Button for adding new employee
-$("#input-user").on("click", function() {
-    // Grab user input
-    firstName = $("#first-name").val().trim();
-    lastName =  $("#last-name").val().trim();
-    name =      firstName + " " + lastName;
-    position =  $("#position").val().trim();
-    department = $("#dept").val().trim();
-    email = $("#email").val().trim();
-    address = $("#home").val().trim();
-    phone = $("#phone").val().trim();
-    assetID = $("#asset-id").val().trim();
 
-    // Create local object to hold employee data
-    var newEmployee = {
-        name: name,
-        position: position,
-        department: department,
-        email: email,
-        address: address,
-        phone: phone,
-        assetID: assetID,
-        created: firebase.database.ServerValue.TIMESTAMP
-    };
+    // Firebase
+    var firstName, lastName, name, position, department, email, address, phone;
 
-    console.log(newEmployee.name);
 
-    // Upload employee data to database
-    firebase.database().ref().push(newEmployee);
+    // Button for adding new employee
+    $("#input-user").on("click", function() {
+        event.preventDefault();
+        //retrieving values from form field
+        firstName = $("#first-name").val().trim();
+        lastName = $("#last-name").val().trim();
+        name = firstName + " " + lastName;
+        position = $("#position").val().trim();
+        department = $("#dept").val();
+        email = $("#email").val().trim();
+        address = $("#home").val().trim();
+        phone = $("#phone").val().trim();
 
-    // Clear input fields
-    $("#first-name").val("");
-    $("#last-name").val("");
-    $("#position").val("");
-    $("#dept").val("");
-    $("#email").val("");
-    $("#home").val("");
-    $("#phone").val("");
-    $("#asset-id").val("");
-});
+        //beginning of alert that asks if the information looks correct and you
+        //are ready to submit
+        // change to modal 
 
-firebase.database().ref().on("child_added", function(childSnapshot, prevChildKey) {
+        var confirmSubmit = confirm("Does everything look okay to you? " + "\n" + "\n" + "Name: " +
+            name + "\n" + "Position: " + position + "\n" + "Department: " + department + "\n" + "Email: " + email +
+            "\n" + "Address: " + address + "\n" + "Phone: " + phone);
 
-    // Create local variables from child object
-    name = childSnapshot.val().name;
-    position = childSnapshot.val().position;
-    department = childSnapshot.val().department;
-    email = childSnapshot.val().email;
-    address = childSnapshot.val().address;
-    phone = childSnapshot.val().phone;
-    assetID = childSnapshot.val().assetID;
+        if (confirmSubmit == false) {
+            return false;
+        }
 
-    $("#people-table > tbody").append("<tr><td>" + name + "</td><td>" + position + "</td><td>" +
-        department + "</td><td>" + email + "</td><td>" + address + "</td><td>" + phone + "</td><td>" + assetID + "</td></tr>");
 
-});
 
-// Logout Button
-$("#logout-button").on("click", function() {
-    event.preventDefault();
-    // var currentUser = Parse.User.current();
+
+
+
+        // Create local object to hold employee data
+        var newEmployee = {
+            name: name,
+            position: position,
+            department: department,
+            email: email,
+            address: address,
+            phone: phone,
+            assetID: "",
+            created: firebase.database.ServerValue.TIMESTAMP
+        };
+
+
+
+        console.log(newEmployee.name);
+
+        // Upload employee data to database
+
+        firebase.database().ref().push(newEmployee);
+
+
+
+        // Clear input fields
+        $("#first-name").val("");
+        $("#last-name").val("");
+        $("#position").val("");
+        $("#dept").val("");
+        $("#email").val("");
+        $("#home").val("");
+        $("#phone").val("");
+
+
+    });
+
+    firebase.database().ref().on("child_added", function(childSnapshot, prevChildKey) {
+
+
+
+        // Create local variables from child object
+        name = childSnapshot.val().name;
+        position = childSnapshot.val().position;
+        department = childSnapshot.val().department;
+        email = childSnapshot.val().email;
+        address = childSnapshot.val().address;
+        phone = childSnapshot.val().phone;
+
+
+
+        $("#people-table > tbody").append("<tr><td>" + name + "</td><td>" + position + "</td><td>" +
+            department + "</td><td>" + email + "</td><td>" + address + "</td><td>" + phone +
+            "</td><td>" + "AS-" + "AssetId" + "</td><td>" + "</td></tr>");
+
+
+    });
+
+
+
+    // Logout Button
+    $("#logout-button").on("click", function() {
+        event.preventDefault();
+        // var currentUser = Parse.User.current();
         // if (currentUser) {
         //     Parse.User.logout();
         //     window.location="Sign_In.html";
         // } else {
-            window.location = "index.html";
-});
+        window.location = "index.html";
+    });
 
 });
+//auto-complete through GoogleMaps API
+
+var placeSearch, autocomplete;
+
+function initAutocomplete() {
+    // Create the autocomplete object, restricting the search to geographical
+    // location types.
+    autocomplete = new google.maps.places.Autocomplete(
+        /** @type {!HTMLInputElement} */
+        (document.getElementById('home')), { types: ['geocode'] });
+
+
+}
+
+
+// Bias the autocomplete object to the user's geographical location,
+// as supplied by the browser's 'navigator.geolocation' object.
+function geolocate() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var geolocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+            var circle = new google.maps.Circle({
+                center: geolocation,
+                radius: position.coords.accuracy
+
+            });
+            autocomplete.setBounds(circle.getBounds());
+        });
+    }
+}
