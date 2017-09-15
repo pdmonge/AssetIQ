2ffeb03e16d50af995a962d85326e2166d22298f
pdmonge <parthamonge@gmail.com>
2017-09-15 13:11:15 -0600

    Working sign-in authentication

--- javascript/auth.js
+++ javascript/auth.js
@@ -1,37 +1,29 @@
-// FIREBASE
-// Initialize Firebase
-var config = {
-  apiKey: "AIzaSyA3CVxnIXyQgnfPZPp1EJsMD1W0xEIGTtw",
-  authDomain: "dubootcamp-group-project1.firebaseapp.com",
-  databaseURL: "https://dubootcamp-group-project1.firebaseio.com",
-  projectId: "dubootcamp-group-project1",
-  storageBucket: "dubootcamp-group-project1.appspot.com",
-  messagingSenderId: "93086047570"
-};
-firebase.initializeApp(config);
-
-function validateInput() {
-	return true;
-}
-
-// Login function
-$('#loginButtonSubmit').click({
-	// Get Login email address and password
-	
-
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  // var errorCode = error.code;
-  // var errorMessage = error.message;
-  // ...
-  console.log('Error: ',error.code,' ',error.message);
-	});
-});
+// AssetIQ
+// Code for index.html
 
 
+$(document).ready(function() {
+  // Login function
+  $('#login-button-submit').click(function(event) {
+    event.preventDefault();
 
+    $('#auth-message').empty();
 
-document.ready(function() {
+    // Get Login email address and password
+    var email = $('#login-input-email').val().trim();
+    var password = $('#login-input-password').val().trim();
 
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function (user) {
+      window.location = 'people.html';
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      $('#auth-message').text(error.message);
+    });
 
+    
+
+    console.log(firebase.UserInfo);
+  });
 });
