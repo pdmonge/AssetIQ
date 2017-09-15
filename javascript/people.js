c0ec59f7a2809c6ec454da25c70f7381e52a3bd3
mollie chacon <mollie.mosley@gmail.com>
2017-09-14 18:58:30 -0600

    removed emp.department info from people.js

--- javascript/people.js
+++ javascript/people.js
@@ -18,7 +18,8 @@
 $(document).ready(function() {
 
 // Firebase
-var firstName, lastName, name, position, department, email, address, phone, assetID;
+// var firstName, lastName, name, position, department, email, address, phone, assetID;
+var firstName, lastName, name, position, email, address, phone, assetID;
 
 // Button for adding new employee
 $("#input-user").on("click", function() {
@@ -27,7 +28,7 @@ $("#input-user").on("click", function() {
     lastName =  $("#last-name").val().trim();
     name =      firstName + " " + lastName;
     position =  $("#position").val().trim();
-    department = $("#dept").val().trim();
+    // department = $("#dept").val().trim();
     email = $("#email").val().trim();
     address = $("#home").val().trim();
     phone = $("#phone").val().trim();
@@ -37,7 +38,7 @@ $("#input-user").on("click", function() {
     var newEmployee = {
         name: name,
         position: position,
-        department: department,
+        // department: department,
         email: email,
         address: address,
         phone: phone,
@@ -54,7 +55,7 @@ $("#input-user").on("click", function() {
     $("#first-name").val("");
     $("#last-name").val("");
     $("#position").val("");
-    $("#dept").val("");
+    // $("#dept").val("");
     $("#email").val("");
     $("#home").val("");
     $("#phone").val("");
@@ -66,14 +67,17 @@ firebase.database().ref().on("child_added", function(childSnapshot, prevChildKey
     // Create local variables from child object
     name = childSnapshot.val().name;
     position = childSnapshot.val().position;
-    department = childSnapshot.val().department;
+    // department = childSnapshot.val().department;
     email = childSnapshot.val().email;
     address = childSnapshot.val().address;
     phone = childSnapshot.val().phone;
     assetID = childSnapshot.val().assetID;
 
+    // $("#people-table > tbody").append("<tr><td>" + name + "</td><td>" + position + "</td><td>" +
+    //     department + "</td><td>" + email + "</td><td>" + address + "</td><td>" + phone + "</td><td>" + assetID + "</td></tr>");
+
     $("#people-table > tbody").append("<tr><td>" + name + "</td><td>" + position + "</td><td>" +
-        department + "</td><td>" + email + "</td><td>" + address + "</td><td>" + phone + "</td><td>" + assetID + "</td></tr>");
+         email + "</td><td>" + address + "</td><td>" + phone + "</td><td>" + assetID + "</td></tr>");
 
 });
 
