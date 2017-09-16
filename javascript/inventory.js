31c8af8d884691108f74bf4423089bcbfbc867a6
mollie chacon <mollie.mosley@gmail.com>
2017-09-15 18:42:53 -0600

    testing column sizing

--- javascript/inventory.js
+++ javascript/inventory.js
@@ -123,10 +123,10 @@ $(document).ready(function () {
 
         var name = (sv.EmployeeKey) ? sv.EmployeeName : 'Unassigned';
 
-        $("#item-list").append('<div class="row" id="' + childSnapshot.key + '"><div class="col-sm-2"><img class="tableImg" src="' + sv.ItemImage +
+        $("#item-list").append('<div class="row" id="' + childSnapshot.key + '"><div class="col-sm-3"><img class="tableImg" src="' + sv.ItemImage +
 
   
-            '" /></div><div class="col-sm-8"><h5 class="tableText">' + sv.ItemDescription +
+            '" /></div><div class="col-sm-7"><h5 class="tableText">' + sv.ItemDescription +
             '</h5></div ><div class="col-sm-2"><h5 class="tableText">' + sv.AssetID +
             // '</h5></div ><div class="col-sm-3"><h5 class="tableText">' + sv.EmployeeName +
             // '</h5></div><div class="col-sm-2"><h5 class="tableText"><button class="deleteButton" data-key="' + childSnapshot.key + '">Delete</button>' +
