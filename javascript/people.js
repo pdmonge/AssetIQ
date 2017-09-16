8cda02bada2b573b32320b2c514fce3d1a98d8f0
pdmonge <parthamonge@gmail.com>
2017-09-15 21:43:03 -0600

    Functionality to populate asset selection dropdown with un-assigned assets

--- javascript/people.js
+++ javascript/people.js
@@ -1,6 +1,7 @@
 //Initialize Firebase through external link
 
 $(document).ready(function() {
+  loadAssetDropdown();
 
   var firstName, lastName, name, position, department, email, address, phone, assetID;
 
@@ -17,9 +18,10 @@ $(document).ready(function() {
     email = $("#email").val().trim();
     address = $("#home").val().trim();
     phone = $("#phone").val().trim();
+    assetID = $("#asset-id-dropdown").val().trim();
     $("#my-modal").modal();
     $(".modal-body").html("Name: " + name + "<br>" + "Position: " + position + "<br>" + "Department: " + department + "<br>" + "Email: " + email +
-      "<br>" + "Address: " + address + "<br>" + "Phone: " + phone);
+      "<br>" + "Address: " + address + "<br>" + "Phone: " + phone + "<br" + "AssetID: " + assetID);
   });
 
   $("#yes").on("click", function() {
@@ -105,3 +107,65 @@ function geolocate() {
     });
   }
 }
+
+var assetList = [];
+function loadAssetDropdown() {
+  // console.log(firebase.database().ref('assets'));
+
+  // Get assets from the database
+  var assetQuery = firebase.database().ref("assets").orderByKey();
+  assetQuery.once("value")
+  .then(function(assetsSnapshot) {
+
+    // Fill the assetList array
+    assetsSnapshot.forEach(function(aAssetSnapshot) {
+      var assetData = aAssetSnapshot.val();
+      // console.log('Asset Data: ',assetData);
+      assetList.push(assetData);
+    });
+
+    // Get employees from the database and create a list of assigned assetIDs
+    var assignedAssetList = [];
+    var employeeQuery = firebase.database().ref("employees").orderByKey();
+    employeeQuery.once("value")
+    .then(function(employeesSnapshot) {
+
+      // Create list of assigned assets
+      employeesSnapshot.forEach(function(aEmployeeSnapshot) {
+        var employeeData = aEmployeeSnapshot.val();
+        console.log('Employee Data: ',employeeData);
+        if (employeeData.assetID != '') {
+          assignedAssetList.push(employeeData.assetID);
+        }
+      }); // employeesSnapshot.forEach
+
+      // Remove asssigned assets from assetList
+      var tempAssetList = [];
+      for (var i=0,len=assetList.length; i<len; i++) {
+        var currentAsset = assetList[i];
+
+        if (assignedAssetList.indexOf(currentAsset.AssetID) === -1) {
+          tempAssetList.push(currentAsset);
+        }
+      }
+      assetList = tempAssetList;
+      loadAssetDropDownFromArray();
+    }); // employeeQuery.then
+  }); // assetQuery.then
+} // function loadAssetDropDown
+
+// Load assetDropDown from assetList array
+function loadAssetDropDownFromArray() {
+  assetDropDown = $('#asset-id-dropdown');
+  assetDropDown.empty();
+  assetDropDown.append('<option value="" disabled selected hidden>Please Select</option>');
+  for (var i = 0,len=assetList.length;i<len;i++) {
+    currentAsset = assetList[i];
+    var newOptionItem = $('<option>');
+    console.log(newOptionItem);
+    newOptionItem.attr('data-index',i);
+    newOptionItem.val(currentAsset.AssetID);
+    newOptionItem.text(currentAsset.AssetID+' '+currentAsset.ItemDescription[0]);
+    assetDropDown.append(newOptionItem);
+  }
+} // function loadAssetDropDownFromArray
